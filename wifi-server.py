e6c82d139597a14f42e1e287ed1cb5e1521950c6
nikhil-robinson <nikhilrobinson2000@gmail.com>
2023-03-26 00:46:34 +0530

    code cleanup

--- wifi-server.py
+++ wifi-server.py
@@ -1,50 +1,81 @@
 import platform
-import os
+import subprocess
 import tkinter as tk
 
-# Create a tkinter window
-root = tk.Tk()
-root.title('Hotspot Creator')
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("WiFi Hotspot Creator")
 
-# Detect the operating system
-os_name = platform.system()
+        # Create input fields
+        ssid_label = tk.Label(self, text="SSID:")
+        ssid_label.grid(row=0, column=0)
+        self.ssid_entry = tk.Entry(self)
+        self.ssid_entry.grid(row=0, column=1)
 
-# Function to create a hotspot
-def create_hotspot():
-    ssid = ssid_entry.get()
-    password = password_entry.get()
-    
-    if os_name == 'Windows':
-        os.system(f'netsh wlan set hostednetwork mode=allow ssid={ssid} key={password}')
-        os.system('netsh wlan start hostednetwork')
-        hotspot_label.config(text='Hotspot enabled on Windows')
-    
-    elif os_name == 'Darwin':
-        os.system(f'sudo networksetup -setairportnetwork en0 {ssid} {password}')
-        hotspot_label.config(text='Hotspot enabled on Mac')
-    
-    elif os_name == 'Linux':
-        os.system(f'sudo  ./home/nikhil/Desktop/Nikhil/wifi-server/lnxrouter --ap wlan0 {ssid} -p {password}')
-        hotspot_label.config(text='Hotspot enabled on Linux')
-    
-    else:
-        hotspot_label.config(text='Unsupported operating system')
-        
-# Create input fields and button
-ssid_label = tk.Label(root, text='SSID:')
-ssid_entry = tk.Entry(root)
-password_label = tk.Label(root, text='Password:')
-password_entry = tk.Entry(root, show='*')
-create_button = tk.Button(root, text='Create Hotspot', command=create_hotspot)
-hotspot_label = tk.Label(root, text='')
+        password_label = tk.Label(self, text="Password:")
+        password_label.grid(row=1, column=0)
+        self.password_entry = tk.Entry(self, show="*")
+        self.password_entry.grid(row=1, column=1)
 
-# Grid the input fields and button
-ssid_label.grid(row=0, column=0)
-ssid_entry.grid(row=0, column=1)
-password_label.grid(row=1, column=0)
-password_entry.grid(row=1, column=1)
-create_button.grid(row=2, column=0, columnspan=2)
-hotspot_label.grid(row=3, column=0, columnspan=2)
+        # Create button to start hotspot
+        start_button = tk.Button(self, text="Start Hotspot", command=self.start_hotspot)
+        start_button.grid(row=2, column=0, columnspan=2)
 
-# Start the tkinter event loop
-root.mainloop()
+    def start_hotspot(self):
+        # Get operating system
+        system = platform.system()
+
+        # Get available Wi-Fi interfaces
+        if system == "Linux":
+            process = subprocess.Popen(["iwconfig"], stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            interfaces = [line.split()[0] for line in output.decode().split("\n") if "IEEE" in line]
+            if len(interfaces) == 1:
+                wifi_interface = interfaces[len(interfaces)-1]
+            else:
+                wifi_interface = tk.simpledialog.askstring("Select Wi-Fi Interface", "Enter the name of the Wi-Fi interface to use:", initialvalue=interfaces[0])
+        elif system == "Darwin":
+            process = subprocess.Popen(["networksetup", "-listallhardwareports"], stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            interfaces = [line.split()[-1] for line in output.decode().split("\n") if "Wi-Fi" in line]
+            wifi_interface = interfaces[0]
+        elif system == "Windows":
+            process = subprocess.Popen(["netsh", "wlan", "show", "drivers"], stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            if "Hosted network supported  : Yes" in output.decode():
+                process = subprocess.Popen(["netsh", "wlan", "show", "interfaces"], stdout=subprocess.PIPE)
+                output, error = process.communicate()
+                interfaces = [line.split(":")[-1].strip() for line in output.decode().split("\n") if "Name" in line]
+                wifi_interface = interfaces[0]
+            else:
+                tk.messagebox.showerror("Error", "This computer does not support creating a hotspot")
+                return
+        else:
+            tk.messagebox.showerror("Error", "Operating system not supported")
+            return
+
+        # Get input values
+        ssid = self.ssid_entry.get()
+        password = self.password_entry.get()
+
+        # Create hotspot
+        if system == "Linux":
+            command = f"sudo ifconfig {wifi_interface} down"
+            subprocess.Popen(command.split())
+            command = f"sudo bash lnxrouter --ap  {wifi_interface} {ssid} -p {password} -c 6"
+            subprocess.Popen(command.split())
+            command = f"sudo ifconfig {wifi_interface} up"
+            subprocess.Popen(command.split())
+        elif system == "Darwin":
+            command = f"sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -z && sudo /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -P createNetwork {ssid} {password} -I {wifi_interface}"
+            subprocess.Popen(command, shell=True)
+        elif system == "Windows":
+            command = f"netsh wlan set hostednetwork mode=allow ssid={ssid} key={password}"
+            subprocess.Popen(command.split())
+            command = f"netsh wlan start hostednetwork"
+            subprocess.Popen(command.split())
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
