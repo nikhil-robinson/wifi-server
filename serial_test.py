0ce99c5237f4ee0cf132a6a918dd7e8ba1c08301
nikhil-robinson <nikhilrobinson2000@gmail.com>
2023-04-11 03:45:57 +0530

    structure changed

--- serial_test.py
+++ serial_test.py
@@ -1,56 +1,74 @@
 import serial.tools.list_ports, sys, time
 import threading
-from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QComboBox, QDialog, QFormLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit, QVBoxLayout, QTabWidget
+from PyQt6.QtCore import Qt, QMetaObject, pyqtSlot
+from PyQt6.QtWidgets import QApplication, QComboBox, QDialog, QFormLayout, QHBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit, QVBoxLayout
 from PyQt6 import QtWidgets
 
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QFormLayout, QLabel, QLineEdit, QPushButton, QComboBox, QTextEdit, QButtonGroup, QHBoxLayout, QRadioButton
+from PyQt6.QtCore import Qt, QProcess
 
 
-class SerialTerminal(QDialog):
+
+
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("Serial Terminal")
+
+        self.form_layout = QFormLayout()
+
 
         # UI widgets
         self.port_combo = QComboBox()
+        ports = [p.device for p in serial.tools.list_ports.comports()]
+        self.port_combo.addItems(ports)
+        self.port_combo.setCurrentIndex(0)
+
         self.baudrate_combo = QComboBox()
-        self.custom_baudrate_input = QLineEdit()
-        self.connect_button = QPushButton("Connect")
-        self.console = QTextEdit()
-        self.send_input = QLineEdit()
-        self.send_button = QPushButton("Send")
-
-        # Populate port combo with available ports
-        self.refresh_ports()
-
-        # Populate baudrate combo with standard baudrates
         self.baudrate_combo.addItems(["9600", "19200", "38400", "57600", "115200"])
+        self.port_combo.setCurrentIndex(0)
 
-        # Layout
-        form_layout = QFormLayout()
-        form_layout.addRow(QLabel("Port:"), self.port_combo)
-        form_layout.addRow(QLabel("Baudrate:"), self.baudrate_combo)
-        form_layout.addRow(QLabel("Custom baudrate:"), self.custom_baudrate_input)
-        self.custom_baudrate_input.setEnabled(False)
-        form_layout.addRow(QLabel(), self.connect_button)
-        form_layout.addRow(QLabel("Console:"), self.console)
 
-        send_layout = QHBoxLayout()
-        send_layout.addWidget(self.send_input)
-        send_layout.addWidget(self.send_button)
-
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(form_layout)
-        main_layout.addLayout(send_layout)
-        self.setLayout(main_layout)
-
-        # Signals
+        self.connect_button = QPushButton("Connect")
         self.connect_button.clicked.connect(self.connect_or_disconnect)
-        self.send_input.returnPressed.connect(self.send)
-        self.send_button.clicked.connect(self.send)
-        self.custom_baudrate_input.textChanged.connect(self.custom_baudrate_changed)
 
-        # Serial connection
+        self.console = QTextEdit()
+        self.console.setReadOnly(True)
+
+
+        self.send_input = QLineEdit()
+        self.send_input.returnPressed.connect(self.send)
+        
+        self.send_button = QPushButton("Send")
+        self.send_button.clicked.connect(self.send)
+
+
+        self.form_layout.addRow(QLabel("Port:"), self.port_combo)
+        self.form_layout.addRow(QLabel("Baudrate:"), self.baudrate_combo)
+
+
+        self.form_layout.addRow(QLabel(), self.connect_button)
+
+
+        self.send_layout = QHBoxLayout()
+        self.send_layout.addWidget(self.send_input)
+        self.send_layout.addWidget(self.send_button)
+
+        self.main_layout = QVBoxLayout()
+        self.main_layout.addLayout(self.form_layout)
+        self.main_layout.addWidget(self.console)
+        self.main_layout.addLayout(self.send_layout)
+
+        # self.setLayout(self.main_layout)
+
+        self.central_widget = QWidget()
+        self.central_widget.setLayout(self.main_layout)
+        self.setCentralWidget(self.central_widget)
+
+        self.setWindowTitle("Serial Terminal")
+        self.setGeometry(100, 100, 400, 300)
+        self.show()
+
+
         self.serial = None
         self.connected = False
 
@@ -67,7 +85,7 @@ class SerialTerminal(QDialog):
         if not self.connected:
 
             port = self.port_combo.currentText()
-            baudrate = int(self.custom_baudrate_input.text() or self.baudrate_combo.currentText())
+            baudrate = self.baudrate_combo.currentText()
             self.serial = serial.Serial(port, baudrate,timeout=1)
             self.connect_button.setText("Disconnect")
             self.connected = True
@@ -79,7 +97,7 @@ class SerialTerminal(QDialog):
 
         else:
             self.connected = False
-            time.sleep(1)
+            self.serial_thread.join()
             self.serial.close()
             self.connect_button.setText("Connect")
             self.connected = False
@@ -114,17 +132,15 @@ class SerialTerminal(QDialog):
             # Sleep for 1 second
             time.sleep(1)
 
-            
     def read_serial(self):
-        while self.connected and self.serial.isOpen():
+        while self.connected:
             data = self.serial.readline()
             if data is not None:
                 self.console.append(data.decode().strip())
 
 
 
-if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
-    window = SerialTerminal()
-    window.show()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
     sys.exit(app.exec())
